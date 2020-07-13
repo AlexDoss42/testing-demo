f5a88e87329789bd07bc431cdef4bf79e5704102
Alex Doss <alex.doss42@gmail.com>
2020-07-13 17:21:24 -0600

    some psuedo code and setting up each individual test

--- tests/exercise1.test.js
+++ tests/exercise1.test.js
@@ -1,5 +1,24 @@
 const exercise = require('../exercise1');
 
 describe('fizzBuzz', () => {
+//Test for throwing Error
+  it('should throw an exception if the type of the input is not a number', () => {
 
+  });
+//Test for Fizzbuzz
+  it('should return FizzBuzz if the number is divisible by 3 AND 5', () => {
+
+  });
+//Test for Fizz
+  it('should return Fizz if the number is divisible by 3', () => {
+
+  });
+//Test for Buzz
+  it('should return Buzz if the number is divisible by 5', () => {
+
+  });
+//Test for number
+  it('should return the number if it is not divisible by 3 or 5', () => {
+
+  });
 });

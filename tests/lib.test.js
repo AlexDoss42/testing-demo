52592083897985c92770407fe47b3eb49f2f91b2
Alex Doss <alex.doss42@gmail.com>
2020-07-12 22:02:07 -0600

    adds our first test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,6 @@
-test('Our first test', () => {
-  
+const lib = require('../lib');
+
+test('absolute - should return a positive number if input is positive', () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
 });
